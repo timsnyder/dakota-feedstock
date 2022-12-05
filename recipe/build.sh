3e723e1173152f9e8d3b66ac850747f22db84174
Tim Snyder <snyder.tim@gmail.com>
2022-12-05 21:21:18 +0000

    Revert "workaround SSL config of dakota.sandia.gov and patch boost::dll::import"
    This reverts commit c969597c029cb6b39fb5adf47b8e87f971047c2d.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-set -xe
-
-# download without SSL to work around https://github.com/orgs/snl-dakota/discussions/12
-curl --insecure -o dakota.tar.gz https://dakota.sandia.gov/sites/default/files/distributions/public/dakota-$PKG_VERSION-public-src-cli.tar.gz
-echo 49684ade2a937465d85b0fc69c96408be38bc1603ed2e7e8156d93eee3567d2f dakota.tar.gz | sha256sum --check
-tar --strip-components=1 -xf dakota.tar.gz
-
-# apply patches using git because you don't have to worry about deducing arguments to patch to do
-# the application the same way as conda-build does...
-# apply the patches the way conda-build would
-# see https://github.com/conda/conda-build/blob/32ac010d584a634fa80fde9877e1756d44025e65/conda_build/source.py#L796-L819
-# I manually did
-#   from conda_build.source import apply_one_patch
-#   from conda_build.config import Config
-#   apply_one_patch('.', os.environ['RECIPE_DIR']+'/patches', os.environ['RECIPE_DIR']+'/patches/link_librt.patch', Config(), False)
-# in the build directory to see that it would do 'patch -Np1 -i <file> --binary` for all the
-# existing patches.  And that it didn't need to force LF or CRLF, it used the 'native' format with
-# --binary
-for p in python tests link_librt boost_dll_import_symbol; do
-  patch -Np1 -i $RECIPE_DIR/patches/${p}.patch --binary
-done
-
 mkdir -p build
 cd build
 
